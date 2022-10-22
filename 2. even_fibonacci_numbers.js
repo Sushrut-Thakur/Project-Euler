53814ebf70b4051cf80356fb6a2256efac23d225
Sushrut Thakur <sushrut_thakur@persistent.com>
2022-10-22 17:09:57 +0530

    Add: Sum of Even Fibonacci Numbers

---
+++ 2. even_fibonacci_numbers.js
@@ -0,0 +1,10 @@
+function fiboEvenSum(n) {
+	let sum = 2, first = 1, second = 2;
+	for(let i = first + second; i<=n; first=second, second = i, i = first + second) {
+	  if(i%2 === 0) {
+		sum += i;
+	  }
+	}
+	console.log(sum);
+	return sum;
+  }
